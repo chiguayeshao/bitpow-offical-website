7a7594218824086e90d322079b0a0b381008cd7c
xiaotian <chiguayeshao@gmail.com>
2023-06-04 17:58:52 +0800

    Update Headers.jsx

--- src/components/headers/Headers.jsx
+++ src/components/headers/Headers.jsx
@@ -10,7 +10,7 @@ const Headers = () => {
                         <p className="text-lg md:text-xl text-coolGray-500 font-medium" data-config-id="auto-txt-3-1">We're building a unique blockchain featuring practical Proof of Work (PoW), enhancing the original Nakamoto Consensus, and using a storage network as the computational powerhouse to offer users lower gas fees</p>
                     </div>
                     <div className="w-full px-4 mb-16 md:mb-0">
-                        <div className="flex flex-wrap">
+                        <div className="flex flex-wrap justify-center">
                             <div className="w-full md:w-auto py-1 md:py-0 md:mr-4"><a className="inline-block py-5 px-7 w-full text-base leading-4 text-green-50 font-medium text-center bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-green-500 rounded-md shadow-sm" href="#" data-config-id="auto-txt-16-1">Try Our Testnet</a></div>
                         </div>
                     </div>
