71fca8ac6505896c3457283c3cc44457cdedaec4
xiaotian <chiguayeshao@gmail.com>
2023-06-04 16:45:23 +0800

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
